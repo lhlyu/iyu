27dce6d3a2027c09eecd73d7f9d755329bf83176
lhlyu <2438211370@qq.com>
2019-10-22 10:12:14 +0800

    add db common function

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/lhlyu/iyu/common"
 	"github.com/lhlyu/iyu/module"
 	"testing"
@@ -16,5 +17,15 @@ func TestRepository(t *testing.T) {
 		return
 	}
 	d := NewDao()
-	d.AddNailOne("置顶", "#0000ff")
+	//d.AddNailOne("置顶", "#0000ff")
+	a := d.ConvertToInterface([]int{1, 2, 3, 4, 5})
+	fmt.Println(a...)
+}
+
+func BenchmarkSprintf(b *testing.B) {
+	d := NewDao()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		d.IntConvertToInterface([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
+	}
 }
