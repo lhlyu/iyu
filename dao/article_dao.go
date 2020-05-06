a3cc4657447a2f1acc1fcf055c5aa40d656124ab
lhlyu <lhl103512@163.com>
2020-05-06 21:33:21 +0800

    update

--- dao/article_dao.go
+++ dao/article_dao.go
@@ -1,68 +1,15 @@
 package dao
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/lhlyu/iyu/common"
-	"github.com/lhlyu/iyu/dao/po"
-	"github.com/lhlyu/iyu/trace"
-	"github.com/lhlyu/yutil/v2"
+    "github.com/lhlyu/iyu/trace"
 )
 
 type ArticleDao struct {
-	trace.BaseTracker
+    BaseDao
 }
 
 func NewArticleDao(tracker trace.ITracker) ArticleDao {
 	return ArticleDao{
-		BaseTracker: trace.NewBaseTracker(tracker),
+        BaseDao: NewBaseDao(tracker),
 	}
-}
-
-func (d ArticleDao) Get(whr string, params ...interface{}) (*po.Article, error) {
-	data := &po.Article{}
-	if err := common.DB.Where(whr, params...).First(data).Error; err != nil {
-		d.Error(err, whr, params)
-		if err == gorm.ErrRecordNotFound {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return data, nil
-}
-
-func (d ArticleDao) Query(whr string, params ...interface{}) ([]*po.Article, error) {
-	var datas []*po.Article
-	if err := common.DB.Where(whr, params...).Find(&datas).Error; err != nil {
-		d.Error(err, whr, params)
-		if err == gorm.ErrRecordNotFound {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return datas, nil
-}
-
-func (d ArticleDao) Add(param *po.Article) error {
-	if err := common.DB.Create(param).Error; err != nil {
-		d.Error(err, yutil.Json.Marshal(param))
-		return err
-	}
-	return nil
-}
-
-func (d ArticleDao) Del(whr string, params ...interface{}) error {
-	data := &po.Article{}
-	if err := common.DB.Where(whr, params...).Delete(data).Error; err != nil {
-		d.Error(err, whr, params)
-		return err
-	}
-	return nil
-}
-
-func (d ArticleDao) Update(param *po.Article) error {
-	if err := common.DB.Model(param).Updates(param).Error; err != nil {
-		d.Error(err, yutil.Json.Marshal(param))
-		return err
-	}
-	return nil
 }
--- dao/dao.go
+++ dao/dao.go
@@ -1,9 +1,11 @@
 package dao
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/lhlyu/iyu/trace"
-	"github.com/pkg/errors"
+    "errors"
+    "github.com/jinzhu/gorm"
+    "github.com/lhlyu/iyu/common"
+    "github.com/lhlyu/iyu/trace"
+    "github.com/lhlyu/yutil/v2"
 )
 
 var (
@@ -20,4 +22,50 @@ func NewBaseDao(tracker trace.ITracker) BaseDao {
 	return BaseDao{
 		BaseTracker: trace.NewBaseTracker(tracker),
 	}
+}
+
+func (d BaseDao) Get(data interface{},whr string,params ...interface{}) error{
+    if err := common.DB.Where(whr,params...).First(data).Error;err != nil{
+        d.Error(err, yutil.Json.Marshal(data),whr,params)
+        if err == gorm.ErrRecordNotFound {
+            return nil
+        }
+        return err
+    }
+    return nil
+}
+
+func (d BaseDao) Query(datas interface{},whr string,params ...interface{}) error{
+    if err := common.DB.Where(whr,params...).Find(datas).Error;err != nil{
+        d.Error(err, yutil.Json.Marshal(datas),whr,params)
+        if err == gorm.ErrRecordNotFound {
+            return nil
+        }
+        return err
+    }
+    return nil
+}
+
+func (d BaseDao) Add(data interface{}) error {
+    if err := common.DB.Create(data).Error; err != nil {
+        d.Error(err, yutil.Json.Marshal(data))
+        return err
+    }
+    return nil
+}
+
+func (d BaseDao) Del(data interface{},whr string, params ...interface{}) error {
+    if err := common.DB.Where(whr, params...).Delete(data).Error; err != nil {
+        d.Error(err, yutil.Json.Marshal(data), whr, params)
+        return err
+    }
+    return nil
+}
+
+func (d BaseDao) Update(data interface{},whr string, params ...interface{}) error {
+    if err := common.DB.Model(data).Where(whr,params...).Updates(data).Error; err != nil {
+        d.Error(err, yutil.Json.Marshal(data), whr, params)
+        return err
+    }
+    return nil
 }
